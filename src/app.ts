520799c5143e3c2cafa0e53cf89b955491aa31ba
ejdeathcrash <ejdeathcrash@gmail.com>
2025-04-19 15:20:32 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.routes';
 import { errorHandler } from './middleware/error.middleware';
 
 const app = express();
 
-app.use(express.json()); // Body parser
+// Enable CORS for all origins
+app.use(cors());
 
-// Routes
+// Or you can customize:
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'https://yourdomain.com'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true,
+// }));
+
+app.use(express.json());
 app.use('/api', routes);
 
+// Error handler
 app.use(errorHandler);
 
 export default app;
